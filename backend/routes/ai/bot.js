f478ef0c6d98bb6a560ac090604d4281b6226dd3
Kiran <kiran.sadara6730@gmail.com>
2025-11-06 18:57:15 +0530

    AI BOT has integerated

---
+++ backend/routes/ai/bot.js
@@ -0,0 +1,101 @@
+import {express ,response } from 'express'
+import dotenv from 'dotenv';
+import {GoogleGenerativeAI ,HarmCategory , HarmBlockThreshold } from '@google/generative-ai'
+import Authentication_token from '../../middlewares/Authentication_middeware.js';
+dotenv.config();
+const Bot_Router = express.Router();
+const API_KEY = process.env.GEMINI_API_KEY 
+const model  = process.env.MODEL_NAME
+
+
+
+const GENERATION_CONFIG = {
+    temperature :0.9,
+    topK:1,
+    topP:1,
+    maxOutputTokens : 4096
+}
+
+const SAFETY_SETTINGS   =[
+    { category: HarmCategory.HARM_CATEGORY_HARASSMENT, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE },
+    { category: HarmCategory.HARM_CATEGORY_HATE_SPEECH, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE },
+    { category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE },
+    { category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE },
+];
+
+
+Bot_Router.post("/content" , Authentication_token , async(req,res)=> {
+
+    try{
+
+        const question = req.body;
+
+
+        const genAI = new GoogleGenerativeAI(API_KEY);
+        const model = genAI.getGenerativeModel({model : model});
+
+
+        const chat =model.startChat({
+            generationConfig:GENERATION_CONFIG,
+            safetySettings:SAFETY_SETTINGS,
+            history:[]
+        })
+
+
+        if(!question || question.length===0){
+              
+            return res.status(400).json({
+                message:"Invalid Question..",
+               
+            })
+        }
+
+        const result =await chat.sendMessage(question);
+
+        if(result.error){
+            return res.status(400).json({
+                message:result.error.message
+            })
+        }
+        
+
+        return res.status(201).json({
+            message:"Output Fetched successfully..",
+            data : result.response.text(),
+             all_data:result.response
+        })
+    }
+    catch(er){
+        return res.status(500).json({
+            message:"Internal Server Error",
+            error:er
+        })
+    }
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Bot_Router;
