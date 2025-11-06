48fd4963e4ce77792738e7e89dc0b64e2fdad88a
Kiran <kiran.sadara6730@gmail.com>
2025-11-06 19:58:38 +0530

    Handling the changes in the form page

--- app/components/NotesForm.tsx
+++ app/components/NotesForm.tsx
@@ -1,4 +1,5 @@
 "use client"
+import axios from "axios";
 import React, { useState, FormEvent } from "react";
 
 function Notes({ isOpen, onClose }: { isOpen?: boolean; onClose?: () => void }) {
@@ -6,12 +7,36 @@ function Notes({ isOpen, onClose }: { isOpen?: boolean; onClose?: () => void }) 
     const[message , setMessage]=useState('');
 
     if (!isOpen) return null;
+
+
     async function handleSubmit(e:any) {
+
+        e.preventDefault();
 
 
         try{
-
             
+
+            const response = await axios.post("http://localhost:5000/api/v1/data/upload-data",{
+                data : note
+            },{
+                withCredentials:true
+            })
+
+            if(response.status==200){
+                setMessage("Notes Added Successfully...")
+
+
+               
+            }
+            else{
+                 
+                setMessage(response.data.message);
+
+                
+
+            }
+
 
         }
         catch(er){
@@ -28,11 +53,18 @@ function Notes({ isOpen, onClose }: { isOpen?: boolean; onClose?: () => void }) 
             }
 
 
-            setTimeout(() => {
-               setNote('');
-              
-            }, 3000)
-            
+           
+
+        }
+
+        finally{
+
+            setTimeout(()=>{
+
+                    setMessage('');
+                    setNote('');
+
+                },2000)
 
         }
         
@@ -40,11 +72,11 @@ function Notes({ isOpen, onClose }: { isOpen?: boolean; onClose?: () => void }) 
     
 
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+        <div className="fixed inset-0 bg-gray-200 bg-opacity-50 flex items-center justify-center z-50">
             
             <div className="bg-white w-full max-w-xl rounded-lg shadow-md p-6 space-y-4">
 
-                <h2 className="font-bold font-2xl ">Add Notes</h2>
+                <h2 className="font font-2xl ">Add Notes</h2>
 
                 <form  className="space-y-3" >
 
@@ -53,11 +85,13 @@ function Notes({ isOpen, onClose }: { isOpen?: boolean; onClose?: () => void }) 
                       value={note}
                       onChange={(e)=> setNote(e.target.value)}
                       placeholder="Write your note...."
-                      className="w-full h-42 border text-sm font-mono rounded p-2 focus:ring focus:ring-blue-300"
+                      className="w-full h-72 border text-sm font-mono rounded p-2 focus:ring focus:ring-blue-600 overflow-auto"
                     
                     />
 
-                    <div className="flex justify-end gap-2 text-lg">
+                    <div className="flex justify-end gap-2 text-sm">
+
+                        <button>AI To Optimize</button>
 
                         <button onClick={onClose} className="px-4 py-2 bg-blue-600 shadow-lg rounded-lg text-white">Cancel</button>
                         <button onClick={handleSubmit} className="px-4 py-2 bg-blue-600 shadow-lg rounded-lg text-white">Save Note</button>
