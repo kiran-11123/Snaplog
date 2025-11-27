5658975fc29e2d2b6886b4567a236fee2f144c57
Kiran <kiran.sadara6730@gmail.com>
2025-11-27 20:15:53 +0530

    redis url done

--- backend/routes/redis/redis-client.js
+++ backend/routes/redis/redis-client.js
@@ -3,7 +3,10 @@ import redis from 'redis'
 import { createClient } from 'redis'
 import logger from '../../utils/logger.js';
 
-const redis_client = createClient();
+const redis_client = createClient({
+    url: process.env.REDIS_URL || 'redis://localhost:6379'
+
+});
 
 
 
